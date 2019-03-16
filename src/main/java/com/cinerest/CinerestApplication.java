435e4f5b166cba8e98f3758ed28fa8ae0f579011
Anderson Marques <andersonmarques.ci@gmail.com>
2019-03-16 18:29:41 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/cinerest/CinerestApplication.java
@@ -0,0 +1,13 @@
+package com.cinerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinerestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinerestApplication.class, args);
+	}
+
+}
