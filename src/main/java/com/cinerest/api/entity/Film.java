775387138aed303d6ae1bc0376b58bb59a4d1cc2
Anderson Marques <andersonmarques.ci@gmail.com>
2019-03-16 18:47:11 -0300

    Criação dos pacotes e entidade Film

---
+++ src/main/java/com/cinerest/api/entity/Film.java
@@ -0,0 +1,32 @@
+package com.cinerest.api.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_films")
+@Getter
+@Setter
+public class Film {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id;
+	
+	public String title;
+	
+	public String summary;
+	
+	@Column(name = "movie_theater")
+	public String movieTheater;
+		
+	public String schedule;
+	
+}
